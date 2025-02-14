0c7ad2be5fe617eea650900291a04e45a0993baf
kayleematic <kayleematic@gmail.com>
2025-02-14 11:26:55 -0600

    additions to covert channel

--- sql-data/things.sql
+++ sql-data/things.sql
@@ -4,12 +4,13 @@ use things;
 -- FILL A TABLE WITH ALGAE
 CREATE TABLE things1 (
     algae VARCHAR(255) NOT NULL,
+    info  VARCHAR(255) NOT NULL,
     PRIMARY KEY (algae)
 );
 
 INSERT INTO things1
 VALUES(
-    "isKing"
+    "isKing", "algae != colons"
 );
 
 CREATE TABLE things2 (
